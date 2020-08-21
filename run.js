6321b3a01190267669a1fb092017be0cb63e208e
Mikan-OwO <68114595+Mikan-OwO@users.noreply.github.com>
2020-08-21 23:46:56 +0900

    Update run.js

--- run.js
+++ run.js
@@ -1,3 +1,18 @@
 $("#btn").on("click", async function() {
-  const id = $("#input[name='uid']").val();
+  const id = $("input[name='uid']").val();
+  fetch("https://mikan-owo.github.io/CasinoBot/test", {
+    method: "POST",
+    headers: {
+      "Content-Type": "text/plain; charset=utf-8"
+    },
+    body: id
+  })
+    .then(res => res.json())
+    .then(res => {
+      if(res.error){
+        $("#result").val(res.error).addClass("result-content-error")
+      }else{
+        $("#result").val(res.result).removeClass("result-content-error")
+      }
+    })
 });
