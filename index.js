89666541f6ab4267c9f165e33675a924217f0d76
MikanOwO <hinata721mikan@gmail.com>
2020-08-23 15:07:42 +0000

    edit

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const discord = require('discord.js');
 const client = new discord.Client();
+const noobs = require('discord.js-noobs');
+const nclient = noobs.Client;
 
-const cron = require('node-cron');
-const fs = require('fs');
-const { inspect } = require('util');
 const ms = require('ms');
+const fs = require('fs');
+const cron = require('node-cron');
+const { inspect } = require('util');
 
 const member = require('./member.json');
 const owners = require('./owner.json');
@@ -17,25 +19,25 @@ const app = express();
 const port = process.env.PORT;
 
 const corsOptions = {
-  "origin": "*",
-  optionsSuccessStatus: 200
-}
+	origin: '*',
+	optionsSuccessStatus: 200
+};
 
-app.use(bodyParser.text({ type: "text/*" }));
-app.use(express.static("public", { extensions: ["html"] }));
+app.use(bodyParser.text({ type: 'text/*' }));
+app.use(express.static('public', { extensions: ['html'] }));
 app.use(cors(corsOptions));
 
-app.post("/test", async (req, res) => {
-  const id = req.body;
-  const user = member.find(m => m.uid === id);
-  res.append("content-type", "application/json");
+app.post('/test', async (req, res) => {
+	const id = req.body;
+	const user = member.find(m => m.uid === id);
+	res.append('content-type', 'application/json');
 
-  if(user) {
-    const coins = user.coin
-    res.send({ result: String(coins) });
- } else {
-    res.send({ error: "IDが間違っています" });
- }
+	if (user) {
+		const coins = user.coin;
+		res.send({ result: String(coins) });
+	} else {
+		res.send({ error: 'IDが間違っています' });
+	}
 });
 
 const commands = [
@@ -44,7 +46,7 @@ const commands = [
 	{ name: 'status', value: '現在のステータスを表示します。(stで省略可能)' },
 	{ name: 'give', value: '他人にアイテムを渡すことができます。' },
 	{
-		name: 'slot',
+		name: 'slot1 / slot2',
 		value: 'スロットを回すことができます。引数にはcoinのBET数を入れてください。'
 	}
 ];
@@ -54,8 +56,10 @@ const prefix = '\\';
 client.on('ready', () => {
 	console.log('起動完了です。');
 	setInterval(() => {
-	  member.forEach(m => m.coin += 1);
-	},(60 * 60 * 1000));
+		member.forEach(m => {
+			m.coin += 1;
+		});
+	}, 60000);
 });
 
 client.on('message', async message => {
@@ -64,6 +68,7 @@ client.on('message', async message => {
 	const search = member.find(m => m.uid === message.author.id);
 	const user = member.find(m => m.uid === String(args[0]));
 	switch (command) {
+    
 		case 'eval':
 			if (owners.includes(message.author.id)) {
 				let evaled;
@@ -77,6 +82,15 @@ client.on('message', async message => {
 				message.reply('ないよっ権(限)ないよぉ〜⤴︎');
 			}
 			break;
+        
+		/*case 'charge':
+			if (owners.includes(message.author.id)) {
+				user.coin += Number(args[1]);
+				message.channel.send('ok');
+			} else {
+				message.reply('ないよっ権(限)ないよぉ〜⤴︎');
+			}
+			break;*/
 
 		case 'help':
 			message.channel.send(
@@ -86,8 +100,18 @@ client.on('message', async message => {
 			);
 			break;
 
+		case 'ping':
+			message.channel.send(client.ws.ping + 'ms');
+			break;
+
 		case 'login':
-		  if(Math.floor((Date.now() - message.author.createdAt) / 86400000) < 7) return message.reply("大人しくあと" + (7 - Math.floor((Date.now() - message.author.createdAt) / 86400000)) + "日間待とうね^^")
+			if (Math.floor((Date.now() - message.author.createdAt) / 86400000) < 7)
+				return message.reply(
+					'大人しくあと' +
+						(7 -
+							Math.floor((Date.now() - message.author.createdAt) / 86400000)) +
+						'日間待とうね^^'
+				);
 			if (!search) {
 				member.push({
 					uid: message.author.id,
@@ -139,7 +163,8 @@ client.on('message', async message => {
 				if (args[1]) {
 					switch (args[1]) {
 						case 'coin':
-						if(args[0] === message.author.id) return message.reply("そうやって負荷かけてっとしばくぞ");
+							if (args[0] === message.author.id)
+								return message.reply('そうやって負荷かけてっとしばくぞ');
 							if (search.coin >= Number(args[2]) && Number(args[2]) >= 1) {
 								search.coin -= Number(args[2]);
 								user.coin += Number(args[2]);
@@ -167,12 +192,12 @@ client.on('message', async message => {
 			}
 			break;
 
-		case 'slot':
+		case 'slot1':
 			if (!search) return message.reply('ログインしてください！');
 			const prob = Math.floor(Math.random() * 100);
 			if (Number(args[0]) && Number(args[0]) >= 1) {
 				if (search.coin >= Number(args[0])) {
-					if (prob <= 20) {
+					if (prob <= 25) {
 						search.coin += Number(args[0]) * 4;
 						message.channel.send(
 							'当たりです！\n' + Number(args[0]) * 4 + 'coinを取得しました！'
@@ -192,10 +217,60 @@ client.on('message', async message => {
 			}
 			break;
 
+		case 'slot2':
+			if (!search) return message.reply('ログインしてください！');
+			if (Number(args[0]) && Number(args[0]) >= 1) {
+				if (search.coin >= Number(args[0])) {
+					const results = [];
+					const item = ['🎃', '👑', '🎁', '🍫'];
+					for (let c = 0; c < 9; c++) {
+						results.push(item[Math.floor(Math.random() * item.length)]);
+					}
+
+					if (
+						(results[0] === results[1] && results[1] === results[2]) ||
+						(results[3] === results[4] && results[4] === results[5]) ||
+						(results[6] === results[7] && results[7] === results[8])
+					) {
+						search.coin += Number(args[0]) * 6;
+						message.channel.send(
+							`${results[0]} | ${results[1]} | ${results[2]}\n${results[3]} | ${
+								results[4]
+							} | ${results[5]}\n${results[6]} | ${results[7]} | ${
+								results[8]
+							}` +
+								'\n当たりです！\n' +
+								Number(args[0]) * 6 +
+								'coinを取得しました！'
+						);
+					} else {
+						message.channel.send(
+							`${results[0]} | ${results[1]} | ${results[2]}\n${results[3]} | ${
+								results[4]
+							} | ${results[5]}\n${results[6]} | ${results[7]} | ${
+								results[8]
+							}` + '\nはずれです...'
+						);
+						search.coin -= Number(args[0]);
+					}
+					fs.writeFile('./member.json', JSON.stringify(member), function(err) {
+						if (err) console.log(err);
+					});
+				} else {
+					message.reply('あなたはそんなにcoinを所持していません。');
+				}
+			} else {
+				message.reply('BETするコインの枚数を指定してください！');
+			}
+			break;
+
 		default:
 			message.reply('どうやらコマンドが違うようです。');
 			break;
 	}
 });
 
+nclient.message('ping', 'pong!');
+
+nclient.login(process.env.DISCORD_BOT_TOKEN);
 client.login(process.env.DISCORD_BOT_TOKEN);
